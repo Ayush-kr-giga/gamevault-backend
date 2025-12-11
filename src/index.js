325c320a21b01ea0a20e658354f45bc416a14d5f
Ayush-kr-giga <kumarayushsharma2@gmail.com>
2025-12-11 19:40:32 +0530

    second

--- src/index.js
+++ src/index.js
@@ -22,5 +22,5 @@ app.use("/api/users", userRoutes);
 app.use("/api/games", gameRoutes);
 app.use("/api/auth", authRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
